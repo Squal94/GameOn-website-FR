79ff4091205ea22a719bacc91dd6766ae29275d9
squal94 <lounis.khadir@wanadoo.fr>
2022-02-09 23:22:02 +0100

    Champ firstname complet terminé

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,7 +12,7 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const btnClose = document.querySelectorAll(".close"); // ajout du lien vers la class "close" pour la fermeture du formulaire
-
+const inputFirstName = document.querySelector("#first"); 
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -30,8 +30,36 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
+
 // Vérification et validation des champs du formulaire .
 
+// Input firstname .
+
+inputFirstName.addEventListener('change', function () {
+  validString(this);
+});
+
+const validString = function(input) {
+  // Reg Exp validation d'un texte .
+  let firstRegExp = new RegExp(
+    '^[a-zA-Z0-9-]{2,99}$', 'g'
+  );
+
+  // test de la valeur de notre input
+  let stringValid = firstRegExp.test(inputFirstName.value);
+  let small = inputFirstName.nextElementSibling;
+
+  // condition pour l'affichage d'un test si les valeurs retrées son erroné
+  if (stringValid){
+    small.style.color = 'green';
+    small.style.fontSize = '0.5em';
+    small.innerHTML = 'Champ Valide.';
+  } else {
+    small.style.color = 'red';
+    small.style.fontSize = '0.5em';
+    small.innerHTML = 'Veuillez entrer 2 caractères ou plus pour le champ du nom !';
+  }
+};
 
 
 
@@ -39,3 +67,8 @@ function closeModal() {
 
 
 
+
+/* email regexp .
+let emailRegExp = new RegExp(
+  '^[a-zA-Z0-9.-_]{2,99}[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$', 'g'
+); */
