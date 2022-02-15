3de88872cb5f2549c9d96cb56429afd1b0161f23
squal94 <lounis.khadir@wanadoo.fr>
2022-02-15 23:05:37 +0100

    fin de la modal felicitation et du p4

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,8 +10,10 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalFelicitaion = document.querySelector("#modal");
 const formData = document.querySelectorAll(".formData");
 const btnClose = document.querySelectorAll(".close"); // ajout du lien vers la class "close" pour la fermeture du formulaire
+const btnCloseFelicitation = document.querySelector(".mondal__felicitaion--ok")
 const inputFirstName = document.querySelector("#first"); 
 const inputLastName = document.querySelector("#last"); 
 const inputEmail = document.querySelector("#email"); 
@@ -21,7 +23,7 @@ const inputLocalisation = document.querySelector(".checked2");
 const inputObligatoire = document.querySelector("#checkbox1");
 const CheckValidation = document.querySelector(".checkbox2-label");
 const reserve = document.querySelector("#reserve");
-
+const btnSubmit = document.querySelector('.btn-submit');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -39,6 +41,10 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
+function closeModalFelicitation (){
+  modalFelicitaion.style.display = "none";
+}
+
 // fonction pour l'affichage des champs valid ou non .
 
 function smallColorRed (input ,string) {
@@ -52,8 +58,23 @@ function smallColorGreen (input) {
   let small = input.nextElementSibling;
   small.style.fontSize = '0.5em';
   small.style.color = 'green';
-  small.innerHTML = 'Valid';
+  small.innerHTML = '';
 }
+
+//launch modal fenetre felicitation !
+
+function launchModalFelicitaion(e) {
+  closeModal();
+  modalFelicitaion.style.display = "block";
+  btnCloseFelicitation.addEventListener("click" , send);
+}
+
+// Fermeture de la modal felicitation et envoi du formulaire
+function send() {
+  modalFelicitaion.style.display = "none";
+  reserve.submit();
+}
+
 
  // Input firstName  .
 
@@ -69,7 +90,7 @@ function smallColorGreen (input) {
         // condition pour l'affichage d'un test si les valeurs retrées son erroné
         if (!stringValid){
           smallColorRed(inputFirstName, 'Veuillez entrer 2 caractères ou plus pour le champ du prénom !') ;
-          return false ;
+          return false ; // les returns son utile pour la condition de fin pour valider le formulaire.
         } else {
           smallColorGreen(inputFirstName) ;
           return true ;
@@ -163,7 +184,7 @@ function smallColorGreen (input) {
 
     function validLocalisation () {
 
-      const local1 = document.querySelector('#location1').checked;
+      const local1 = document.querySelector('#location1').checked; 
       const local2 = document.querySelector('#location2').checked;
       const local3 = document.querySelector('#location3').checked;
       const local4 = document.querySelector('#location4').checked;
@@ -201,41 +222,14 @@ function smallColorGreen (input) {
 
 
         
-reserve.addEventListener("submit" , validate);
+  btnSubmit.addEventListener("click" , validate);
 
     function validate(e) {
     if( validString(inputFirstName) && validString2(inputLastName) && validEmail(inputEmail) && validDate(inputBirthdate) && validQuantity(inputQuantity) && validLocalisation(inputLocalisation) && validLu(inputObligatoire)) {
-        reserve.submit();
-        alert("Votre formulaire a bien été envoyé");
+        e.preventDefault();
+        launchModalFelicitaion();
       } else {
          alert("Vous devez remplir tous les champs");
         e.preventDefault();
       }
     };
-
-
-  
-
-
-
-/*
-
-  inputEmail.addEventListener('change', function () {
-    validEmail();
-  });
-
-  const validEmail = function() {
-    let emailRegExp = new RegExp(
-      '^[a-zA-Z0-9.-_]{2,99}[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$', 'g'
-    );
-      let emailValid = emailRegExp.test(inputEmail.value);
-    
-      if (!emailValid ){
-        smallColorRed(inputEmail, 'Veuillez entrer une adresse email valide (xyz@exemple.com) !') ; 
-      } else {
-        smallColorGreen (inputEmail);
-      }
-      return emailValid;
-  };
-
-*/
